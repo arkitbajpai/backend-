bfa870bf5e145bdaca96613a6c35fce021d7368b
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-07-20 22:25:28 +0530

    nested vlock up

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
 import { access } from "fs"
 import { subscribe } from "diagnostics_channel"
+import mongoose from "mongoose"
 
 const generateAccessTokenAndRefreshTokens=async(userId)=>{
     try{
@@ -304,7 +305,49 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
     return res.status(200).json(
         new ApiResponse(200,channel[0],"User Channel fetched successfully")
     )
-})
+    })
+
+    const getWatchHistory= asyncHandler(async(req,res)=>{
+        const user=await User.aggregate([
+         {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+         },{
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[{
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullName:1,
+                                    username:1,
+                                    avatar:1
+                                }
+                            }
+                        ]
+
+                    }
+            },{
+                $addFields:{
+                    owner:{
+                        $first:"$owner"
+                    }
+                }
+            }]
+
+            }
+         }
+        ])
+    })
 
 export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword, getCurrentUsers,updateAccountDetails,
     updateUserAvatar,updateUserCoverImage, }
