862bdc762cff6072b0868863271fb424bd02a080
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-07-22 23:15:08 +0530

    created models

---
+++ src/models/likes.model.js
@@ -0,0 +1,22 @@
+import mongoose,{Schema} from "mongoose";
+
+const likesSchema= new  Schema({
+video:{
+    type: Schema.Types.ObjectId,
+    ref: 'Video'
+},
+comment:{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment'
+}, tweet:{
+    type: Schema.Types.ObjectId,
+    ref: 'Tweet'
+},
+likedBy:{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+}
+
+})
+
+export const likes = mongoose.model('likes', likesSchema);
