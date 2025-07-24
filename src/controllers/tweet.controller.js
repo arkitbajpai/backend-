9e9846b10b3e44db4fc7e0ceb38e8249b788cb45
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-07-24 15:47:42 +0530

    enhance delete tweet functionality with error handling for invalid ID, not found, and authorization

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { isNativeError } from "node:util/types"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
@@ -75,7 +76,23 @@ const updateTweet = asyncHandler(async (req, res) => {
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+
+    const {tweetId} = req.params;
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet ID");
+    }
+    const tweetToBeDeleted = await Tweet.findById(tweetId);
+    if(!tweetToBeDeleted){
+        throw new ApiError(404, "Tweet not found");
+    }
+    if(tweetToBeDeleted.owner.toString()!== req.user._id.toString()){
+        throw new ApiError(403, "You are not authorized to delete this tweet");
+    }
+    const deletedTweet = await tweetToBeDeleted.deleteOne();
+    return res.status(200).json(new ApiResponse(200, deletedTweet, "Tweet deleted successfully"));
+
 })
+
 
 export {
     createTweet,
